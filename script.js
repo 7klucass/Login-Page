4ddfbdd521b40c913ef7fd99f8b47d1c1a43716c
lucas <dossantos.lucas@eaportal.org>
2024-11-09 23:22:11 -0300

    palmeiras maior que santos

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ document.querySelectorAll('.toggle').forEach(toggle => {
       document.querySelector('.wrapper').classList.toggle('flip');
    });
 });
+
+
+const loginButton = document.querySelector('#button-id');
+
+if (loginButton) {
+  loginButton.addEventListener('click', async () => {
+
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const response = await fetch('http://localhost:5000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+       email, senha
+      })
+    });
+
+    if (response.ok) {
+      const result = await response.text();
+      alert(result);
+    } else {
+      alert('Usuário ou senha inválidos!'); 
+    }
+  });
+}
